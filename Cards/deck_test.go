0476d6f8223db535cab30192d5ead7ea646f3013
MSKU786 <manishsingh332000@gmail.com>
2024-10-29 11:39:34 +0530

    added useful tests

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDec(t *testing.T) {
 		d := newDeck();
@@ -8,4 +11,27 @@ func TestNewDec(t *testing.T) {
 		if (len(d) != 16) {
 				t.Errorf("Excpected deck length is 16 but we got %v", len(d));
 		}
+
+		if (d[0] != "Ace of Spades") {
+			  t.Errorf("Expected Ace of spades but we got %v", d[0]);
+		}
+
+		if (d[len(d)-1] != "Four of Clubs") {
+				t.Errorf("Expected Four of Clubs but we got %v", d[len(d)-1])
+		}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting");
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting");
+
+	loadedDeck := newDeckFromFile("_decktesting");
+
+	if (len(loadedDeck) != 16) {
+			t.Errorf("Expected 16 cards from file deck, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting");
 }
