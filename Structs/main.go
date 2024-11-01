19c32a28178d003ecf210981a4cad969b89ab2e7
MSKU786 <manishsingh332000@gmail.com>
2024-11-01 10:29:45 +0530

    Added different funciton on person struct

--- Structs/main.go
+++ Structs/main.go
@@ -2,12 +2,36 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipcode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	parth := person{firstName: "Parth", lastName: "Amola"}
-	fmt.Println(parth);
+
+	parth := person{
+		firstName: "Parth", 
+		lastName: "Amola",
+		contactInfo: contactInfo{
+			email: "parthamola@gaa.com",
+			zipcode: 23422,
+		},
+	}
+	
+	parthPointer := &parth;
+	parthPointer.updateName("Parthuli");
+	parth.print()
+}
+
+func (p person ) print () {
+	fmt.Printf("%+v", p);
+}
+
+func (personToPointer *person) updateName(newFirstName string) {
+	*&personToPointer.firstName = newFirstName;
 }
