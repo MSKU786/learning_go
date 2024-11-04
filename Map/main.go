c341d396be30ed7f976bd9ee3ac00da3a522c7bc
MSKU786 <manishsingh332000@gmail.com>
2024-11-04 10:40:41 +0530

    manipulating map

--- Map/main.go
+++ Map/main.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main () {
+
+	/*  How to intialize an empty map both the methods will intialize an empty map
+
+		colors := make(map[string]string)
+		var colors map[string]string
+
+	*/
 		colors := map[string] string {
 			"red" : "ff0000",
 			"green": "#4fbf45",
 		}
+
+		// add a value in map
+		colors["blue"] = "#00ff00"
+
+		// delete a value in map
+		delete(colors, "green");
 
 		fmt.Println(colors);
 }
