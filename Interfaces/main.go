862bdeefa67a64808d8ff4c27d99193770d5c51f
MSKU786 <manishsingh332000@gmail.com>
2024-11-07 11:47:30 +0530

    Added interface bot accepting bot spanish and english bot type for print function

--- Interfaces/main.go
+++ Interfaces/main.go
@@ -1,20 +1,31 @@
 package main
 
-type englishBot struct {}
-type spanishBot struct {}
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
 
 func main() {
-
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb);
+	printGreeting(sb);
 }
 
-func (eb englishBot) getGreeting () string {
-		// Customer login specific to english bot
-		return "Hi! there"
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting());
 }
 
+func (englishBot) getGreeting() string {
+	// Customer login specific to english bot
+	return "Hi! there"
+}
 
-func (eb spanishBot) getGreeting () string {
+func (spanishBot) getGreeting() string {
 	// Customer login specific to spanish bot
 	return "Hola!"
 }
-
