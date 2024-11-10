2954f1c0c2e398443489a320b3c484b7d9bc3002
MSKU786 <manishsingh332000@gmail.com>
2024-11-10 15:36:53 +0530

    Updated comments

--- Interfaces/http/main.go
+++ Interfaces/http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -18,10 +19,13 @@ func main() {
 
 	//Reading data through the reader interface and read function
 
+	// First way to output data using reader interface
 	bs := make([]byte, 99999)
 
 	resp.Body.Read(bs);
 
 	fmt.Println(string(bs))
 
+	//Using wirte  2nd way to use io copy function to use write interface interally
+	io.Copy(os.Stdout, resp.Body);
 }
