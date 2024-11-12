984127714975108c64555d98ba2845af0611105f
MSKU786 <manishsingh332000@gmail.com>
2024-11-12 11:58:14 +0530

    Assignment 2 completed

--- assignment2/assignment2.go
+++ assignment2/assignment2.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	args := os.Args;
-	fileName := args[2];
 
-	// if (fileName == nil) {
-	// 	os.Exit(1)
-	// }
+	if (len(args) < 2) {
+		os.Exit(1);
+	}
 		
-	fmt.Println(fileName)
+	fileName := args[1];
+		
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1);
+	}
+
+	os.Stdout.Write(data)
+
+	f, err := os.Open(fileName);
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1);
+	}
+
+	io.Copy(os.Stdout, f);
 }
