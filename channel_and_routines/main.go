39cf08568d5ef2ade150d558d9c1872914b66432
MSKU786 <manishsingh332000@gmail.com>
2024-11-16 09:58:11 +0530

    Added routines

--- channel_and_routines/main.go
+++ channel_and_routines/main.go
@@ -13,19 +13,24 @@ func main() {
 		"http://stackoverflow.com",
 	}
 
+	c := make(chan string)
+
 	for _,l := range(links) {
-			go checkLink(l);
+			go checkLink(l, c);
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 
 	_, err := http.Get(link);
 
 	if (err != nil) {
 		fmt.Println(link, "might be down!");
+		c <- "Might be down I think"
 		return;
 	}
 
 	fmt.Println(link, "is up");
+	c <- "Yep it's up!"
+
 }
