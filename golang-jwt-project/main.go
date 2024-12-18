77eeb66afc3ce092cc37dd85bb280a8d16a6cdac
MSKU786 <manishsingh332000@gmail.com>
2024-12-18 07:00:15 +0530

    Initialize main.go with basic routes

--- golang-jwt-project/main.go
+++ golang-jwt-project/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	routes "golang-jwt-project/routes"
 	"os"
-	"github.c"
+
+	"github.com/gin-conic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 
 func main() {
+		port := os.Getenv("PORT")
 
+		if port == "" {
+			port = "8000"
+		}
+
+		router := gin.New()
+		router.Use(gin.Logger())
+
+		routes.AuthRoutes(router);
+		routes.UserRoutes(router);
+
+		router.get('/api/v1', func(c *gin.Context) {
+			
+		})
 }
