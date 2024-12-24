fa55ae40a2c4207e5075d8a887ce4dfc5980af01
MSKU786 <manishsingh332000@gmail.com>
2024-12-24 10:05:01 +0530

    Added user sign up function

--- golang-jwt-project/controllers/userController.go
+++ golang-jwt-project/controllers/userController.go
@@ -4,20 +4,78 @@ import (
 	"context"
 	"golang-jwt-project/database"
 	"golang-jwt-project/models"
+	"log"
 	"net/http"
 
 	"golang-jwt-project/helpers"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
 var validate = validator.New()
 
-func Singup() {
+func Singup() gin.HandlerFunc{
+		return func(c *gin.Context) {
+			var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+			var user models.User
+			if err := c.BindJSON(&user); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
 
+			if err := validate.Struct(user); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+
+			count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
+			defer cancel()
+
+			if err != nil {
+				log.Panic(err);
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while checking for the user"})
+				return
+			}
+
+			if count > 0 {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "User email already exists"})
+				return
+			}
+
+			count,err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+
+			if err != nil {
+				log.Panic(err);
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while checking for the user"})
+				return
+			}
+
+			if count > 0 {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "User phone number already exists"})
+				return
+			}
+
+			user.Password, _ = helpers.HashPassword(user.Password)
+			user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			user.ID = primitive.NewObjectID()
+			user.UserId = user.ID.Hex()
+
+			result, insertErr := userCollection.InsertOne(ctx, user)
+			defer cancel()
+
+			if insertErr != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while creating user"})
+				return
+			}
+
+			c.JSON(http.StatusOK, result)
+		}  
 }
 
 func Login() {
