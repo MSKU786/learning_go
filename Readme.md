eec04b12437463a2f594c3b964769ae60b9dcb44
MSKU786 <manishsingh332000@gmail.com>
2025-03-02 10:40:55 +0530

    Differrnt type of print commands

--- Readme.md
+++ Readme.md
@@ -11,3 +11,11 @@ Note: Use pointers to changes these in a function
 
 slices, map, channels, pointers, function
 Note: No need to pass as pointer
+
+### Notes
+
+# Use of go.sum and go.mod
+
+- go.mod is critical for defining your project's dependencies.
+- go.sum ensures the dependencies are downloaded correctly and consistently.
+- Do not remove them unless absolutely necessary and you know what you're doing. If you’re troubleshooting, regenerate them using go mod tidy.
---
+++ printing_formatting/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	age := 35;
+	name := "Ninja"
+	// Print doesn't start the new line for new line you have to add \n 
+	fmt.Print("Hello .");
+	fmt.Print("World !")
+	
+
+	// Println always end the line
+	fmt.Println("Hello boys!")
+	fmt.Println("Let's start with today's go lesson")
+
+	// Below is the example for passing variable in the string
+	fmt.Println("My age is", age, "and my name is", name);
+
+
+	// Printf (formatted Strings) %v = formated specifiers 
+	//
+	fmt.Printf("my age is %v and my name is %v \n", age, name);
+	fmt.Printf("my age is %q and my name is %q \n", age, name);
+	fmt.Printf("age is type of %T \n", age);
+	fmt.Printf("you scored %f points \n", 223.334);
+	fmt.Printf("you scored %0.1f points \n", 223.3233);
+	/*
+	my age is 35 and my name is Ninja
+	my age is '#' and my name is "Ninja"
+	age is type of int
+	you scored 223.334000 points
+	you scored 223.3 points
+*/
+}
